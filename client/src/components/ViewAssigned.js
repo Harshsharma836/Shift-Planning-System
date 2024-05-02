2431f109f9672c2c50b78bb89570a56b68cb34ba
Harsh Sharma <Harshsharma1421@gmail.com>
2024-05-02 23:51:10 +0530

    fix-view-assigned-shift

--- client/src/components/ViewAssigned.js
+++ client/src/components/ViewAssigned.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { DateTime } from 'luxon';
 
 const ViewAssigned = () => {
   const [shifts, setShifts] = useState([]);
@@ -8,7 +9,7 @@ const ViewAssigned = () => {
     const fetchShifts = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('http://localhost:5000/api/user/shifts', {
+        const response = await axios.get('http://localhost:5000/user/shifts', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -31,16 +32,18 @@ const ViewAssigned = () => {
             <th>Date</th>
             <th>Start Time</th>
             <th>End Time</th>
-            <th>Timezone</th>
+            <th>Employee Timezone</th>
+            <th>Admin Timezone</th>
           </tr>
         </thead>
         <tbody>
           {shifts.map((shift) => (
             <tr key={shift._id}>
-              <td>{shift.date}</td>
-              <td>{shift.startTime}</td>
-              <td>{shift.endTime}</td>
+              <td>{DateTime.fromISO(shift.date).toLocaleString(DateTime.DATE_FULL)}</td>
+              <td>{DateTime.fromISO(shift.startTime).toLocaleString(DateTime.TIME_24_SIMPLE)}</td>
+              <td>{DateTime.fromISO(shift.endTime).toLocaleString(DateTime.TIME_24_SIMPLE)}</td>
               <td>{shift.timezone}</td>
+              <td>{shift.toTimezone}</td>
             </tr>
           ))}
         </tbody>
